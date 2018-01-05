13532ae69605604b3b6f9b378e8f412c090d5520
Adam Johns <robert.adam.johns@gmail.com>
2018-01-05 00:09:36 -0600

    Remove unnecessary dependency

--- calculate.js
+++ calculate.js
@@ -1,4 +1,5 @@
 var fetch = require('node-fetch');
+var subreddits = require('./subreddits.json');
 
 async function getTop100MarketCapCoins() {
     try {
@@ -6,13 +7,14 @@ async function getTop100MarketCapCoins() {
         if (response.ok) {
             let coins = await response.json();
             coins.forEach(function(coin) {
+                console.log(coin.name + ' ' + coin.market_cap_usd);
 
                 // get subreddit name from subreddits.json
+                console.log(subreddits[coin.name]);
 
                 // call that subreddit's about.json
 
                 // parse the subscribers key from about.json
-                console.log(coin.name + ' ' + coin.market_cap_usd);
             });
         }
     } catch (error) {
