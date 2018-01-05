98f6001d2b4d09815a17242aa28457fcbda21c69
Adam Johns <robert.adam.johns@gmail.com>
2018-01-05 00:27:22 -0600

    Progress

--- calculate.js
+++ calculate.js
@@ -6,16 +6,34 @@ async function getTop100MarketCapCoins() {
         let response = await fetch('https://api.coinmarketcap.com/v1/ticker/');
         if (response.ok) {
             let coins = await response.json();
-            coins.forEach(function(coin) {
-                console.log(coin.name + ' ' + coin.market_cap_usd);
+            coins.forEach(function (coin) {
+                // console.log(coin.name + ' ' + coin.market_cap_usd);
 
                 // get subreddit name from subreddits.json
-                console.log(subreddits[coin.name]);
+                let subreddit = subreddits[coin.name];
+                if (typeof subreddit != 'undefined') {
+                    console.log(subreddits[coin.name]);
+                    getSubredditSubscribers(subreddit);
+                }
 
                 // call that subreddit's about.json
 
                 // parse the subscribers key from about.json
             });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getSubredditSubscribers(subreddit) {
+    try {
+        let response = await fetch('https://www.reddit.com/r/' + subreddit + '/about.json');
+        if (response.ok) {
+            let about = await response.json();
+            let data = about.data;
+            let numSubscribers = data.subscribers;
+            console.log(numSubscribers);
         }
     } catch (error) {
         console.log(error);
